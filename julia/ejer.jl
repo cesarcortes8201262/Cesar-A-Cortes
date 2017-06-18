2a9ece8966c49cc7f423d2d956df1712d1a1b3ae
vhmar <marvh@geociencias.unam.mx>
2017-06-18 18:30:41 -0500

    Function ejemplo

--- julia/ejer.jl
+++ julia/ejer.jl
@@ -1,12 +1,11 @@
 function my(a::String)
-	import Base.using	
-	println(a)
+	
 	if !haskey(Pkg.installed(), a)
 	    Pkg.add(a)
-	    using(a)
+	    #using(a)
 	else
           println("Ya lo tengo-Cargando" $a)
-	  using(a)
+	  #using(a)
         end
 end
 
